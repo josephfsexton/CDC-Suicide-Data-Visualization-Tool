db1bdc26b4e5433777e2a31cf32015841d7a79c5
josephfsexton <75770591+josephfsexton@users.noreply.github.com>
2021-10-08 23:52:43 -0500

    Update compiler-app.R

--- compiler-app.R
+++ compiler-app.R
@@ -9,45 +9,129 @@
 # RASHMIS PATH "/Users/rashmijha/Desktop/suicide project/VH8/all_suicides.csv"
 #suicides <- read.csv("/Users/rashmijha/Desktop/suicide project/VH8/all_suicides.csv")
 
-library(dplyr)
 library(shiny)
+library(dplyr)
+
+suicides <- read.csv("C:\\rProjects\\suicide\\suicides2.0\\all_suicides.csv")
+
+head(suicides$race_cat)
 
 races <- c("White", "Black", "American Indian / Alaskan Native (AIAN)",
            "Asian", "Mixed", "Pacific Islander")
 sexes <- c("Male", "Female")
 ethnicities <- c("Hispanic", "Non-Hispanic")
-race_code = list('White'='white', 'Black'='black', 'American Indian / Alaskan Native (AIAN)'='aian',
-                 'Asian'='asian', 'Mixed'='mixed', 'Pacific Islander'='pacific')
-sex_code = list('Male'='M', 'Female'='F')
-ethnic_code = list('Hispanic'='hispanic', 'Non-Hispanic'='non-hispanic')
+demographics <- c("Sex", "Age", "Race", "Ethnicity", "Nationality", "Marital Status")
+
+ui <- navbarPage("U.S. Suicide Compiler",
+                 tabPanel("HOME",
+                          fluidPage(
+                              titlePanel("Welcome to the U.S. Suicide Compiler!"),
+                              fluidRow(
+                                  column(9, "Suicide varies by population, but basic demographic
+                                         questions remain to be answered. The U.S. Suicide Compiler
+                                         brings together data on your specific demographic of interest,
+                                         using data from the Centers for Disease Control and Prevention,
+                                         to look at intra-population differences. For instance, age is a
+                                         risk factor for White males, but not Black females. Our website
+                                         lets you plot differences by any of 70+ variables of interest,
+                                         including race, ethnicity, means of suicide, place of death,
+                                         age, and a bunch more.")
+                              )
+                          )
+                 ),
+                 tabPanel("SELECT SAMPLE",
+                          fluidPage(
+                              titlePanel("First, select the groups to include in your sample."),
+                              fluidRow(
+                                  column(9, "Later on, you'll do the actual comparison. You might look at how
+                                  age relates to suicide rate, and how this relation varies by sex. But what if
+                                  you're specifically interested in this potential sex difference in the Black
+                                  population? Then, here, you'd select Black under the Demographic -> Race dropdown.
+                                  
+                                  By default, we'll assume you're interested in all people who died by suicide."
+                                  )
+                              ),
+                              sidebarLayout(
+                                  sidebarPanel(
+                                      selectInput("Parameters", "What demographics are you concerned with?",
+                                                  demographics, multiple=TRUE)
+                                  ),
+                                  mainPanel(
+                                      dataTableOutput("data")
+                                  )
+                              )
+                          )
+                 )
+)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-    # Application title
-    titlePanel("U.S. Suicide Compiler"),
-    
-    # Sidebar with a selection input for demographic parameters
-    sidebarLayout(
-        sidebarPanel(
-            checkboxGroupInput("race", "Race of interest?", races, selected=races),
-            checkboxGroupInput("sex", "Sex of interest?", sexes, selected=sexes),
-            sliderInput("age", "Age range of interest?", value=c(1,120), min=1, max=120),
-            checkboxGroupInput("ethnicity", "Ethnic status of interest?", ethnicities, selected=ethnicities),
-        ),
-        mainPanel(
-            dataTableOutput("data")
-        )
-    )
+ui <- navbarPage("U.S. Suicide Compiler",
+                 tabPanel("HOME",
+                          fluidPage(
+                              titlePanel("Welcome to the U.S. Suicide Compiler!"),
+                              fluidRow(
+                                  column(9, "Suicide varies by population, but basic demographic
+                                         questions remain to be answered. The U.S. Suicide Compiler
+                                         brings together data on your specific demographic of interest,
+                                         using data from the Centers for Disease Control and Prevention,
+                                         to look at intra-population differences. For instance, age is a
+                                         risk factor for White males, but not Black females. Our website
+                                         lets you plot differences by any of 70+ variables of interest,
+                                         including race, ethnicity, means of suicide, place of death,
+                                         age, and a bunch more.")
+                              )
+                          )
+                 ),
+                 tabPanel("SELECT SAMPLE",
+                          fluidPage(
+                              titlePanel("First, select the groups to include in your sample."),
+                              fluidRow(
+                                  column(9, "Later on, you'll do the actual comparison. You might look at how
+                                  age relates to suicide rate, and how this relation varies by sex. But what if
+                                  you're specifically interested in this potential sex difference in the Black
+                                  population? Then, here, you'd select Black under the Demographic -> Race dropdown.
+                                  
+                                  By default, we'll assume you're interested in all people who died by suicide."
+                                  )
+                              ),
+                              sidebarLayout(
+                                  sidebarPanel(
+                                      selectInput("Parameters", "What demographics are you concerned with?",
+                                                  demographics)
+                                  ),
+                                  mainPanel(
+                                      dataTableOutput("data")
+                                  )
+                              )
+                          )
+                 )
 )
+    # Application title
+    #tabPanel("Download Your Dataset", fluidPage(
+    #    titlePanel("Select your parameters and download your dataset."),
+    #    sidebarPanel(
+    #        checkboxGroupInput("race", "Race of interest?", races, selected="White"),
+    #        checkboxGroupInput("sex", "Sex of interest?", sexes),
+    #        sliderInput("age", "Age range of interest?", value=c(1,120), min=1, max=120),
+    #        checkboxGroupInput("ethnicity", "Ethnic status of interest?", ethnicities),
+    #    ),
+    #    mainPanel(
+    #        dataTableOutput("data")
+    #    ))
+    #),
+    #tabPanel("Data Visualization", 1)
+#)
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-    
+    race_code = list('White'='white', 'American Indian / Alaskan Native (AIAN)'='aian',
+                     'Asian'='asian', 'Mixed'='mixed', 'Pacific Islander'='pacific')
+    sex_code = list('Male'='M', 'Female'='F')
+    ethnic_code = list('Hispanic'='hispanic', 'Non-Hispanic'='non-hispanic')
     output$data <- renderDataTable({
-        
-        filter(suicides, race_cat %in% race_code[input$race], sex %in% sex_code[input$sex], 
-               age>=input$age[1], age<=input$age[2], hispanic_cat %in% ethnic_code[input$ethnicity])
-        })
+        filter(suicides, race_cat==race_code[input$race], sex==sex_code[input$sex],
+             hispanic_cat==ethnic_code[input$ethnicity], age>=input$age[1], age<=input$age[2])
+    })
 }
 
 
